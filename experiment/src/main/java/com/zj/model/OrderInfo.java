8fc3d7d954e36e958ea8f5f79e801b6bdd1ecc5f
SuShaohua <sshzh90@gmail.com>
2018-10-19 20:03:32 +0800

    modify

---
+++ experiment/src/main/java/com/zj/model/OrderInfo.java
@@ -0,0 +1,8 @@
+package com.zj.model;
+
+import lombok.Data;
+
+@Data
+public class OrderInfo {
+    private String orderId;
+}
---
+++ experiment/src/main/java/com/zj/model/OrderItem.java
@@ -0,0 +1,7 @@
+package com.zj.model;
+
+import lombok.Data;
+
+@Data
+public class OrderItem {
+}
